c96364b6ce404bbfa83780e26d8ed0a93dda6c48
Michael Seydel <micseydel@gmail.com>
2016-05-21 00:05:05 -0700

    Added instantiable player classes for command line use.
    Passive player will take the path of least resistance. Useful for testing.
    Renamed human player to command line player, since that's really what I'll be doing and the I/O is explained better. Added cardToDiscardForHandSizeLimit method as well as chooseAction to player. Removed opponent from player, though may need to re-add it later on.

--- src/main/scala/yugioh/Player.scala
+++ src/main/scala/yugioh/Player.scala
@@ -1,5 +1,6 @@
 package yugioh
 
+import yugioh.action.Action
 import yugioh.card.Card
 
 import scala.collection.mutable.ListBuffer
@@ -7,32 +8,74 @@ import scala.collection.mutable.ListBuffer
 trait Player {
   val name: String
   val deck: Deck
-  val field: Field
+  val field: Field = new FieldImpl
   val grave: ListBuffer[Card] = new ListBuffer[Card]
   val banished: ListBuffer[Card] = new ListBuffer[Card]
   val hand: ListBuffer[Card] = new ListBuffer[Card]
-  val opponent: Player // TODO I don't like this
 
   var lifePoints: Int = Player.InitialLifePoints
+
+  /**
+    * @param actions must contain at least one thing, typically an option to pass
+    */
+  def chooseAction(actions: Seq[Action])(implicit gameState: GameState, phase: Phase): Action
+
   def draw(): Unit = hand.append(deck.fromTop())
+
+  def cardToDiscardForHandSizeLimit: Card
 }
 
 object Player {
   val InitialLifePoints = 8000
 }
 
-/**
-  * TODO: this should not be abstract
-  */
-abstract class HumanPlayer(val name: String) extends Player {
+class CommandLineHumanPlayer(val name: String) extends Player {
+  override val deck: Deck = new TestDeck(this) // TODO: be more than just a stub
 
+  override def chooseAction(actions: Seq[Action])(implicit gameState: GameState, phase: Phase) = {
+    if (actions.size == 1) {
+      val action = actions.head
+      println(s"Action $action was only option, taking implicitly.")
+      action
+    } else {
+      select("Select an action:", actions)
+    }
+  }
+
+  override def cardToDiscardForHandSizeLimit: Card = {
+    select("Select a card to discard:", hand)
+  }
+
+  /**
+    * Ask the user for a specific element of a sequence.
+    */
+  def select[A](prompt: String, options: Seq[A]): A = {
+    println(prompt)
+
+    for ((action, i) <- options.zipWithIndex) {
+      println(s"($i) $action")
+    }
+
+    print("> ")
+    var choice = scala.io.StdIn.readInt()
+    while (choice < 0 || choice >= options.size) {
+      println("Not a valid option.")
+      print("> ")
+      choice = scala.io.StdIn.readInt()
+    }
+
+    options(choice)
+  }
 }
 
 /**
   * Useful for testing purposes.
-  * TODO: this should not be abstract
+  *
+  * When given an option like discarding for hand size or choosing an action, will just select the first.
   */
-abstract class PassivePlayer() extends Player {
+class PassivePlayer extends Player {
   override val name = "PassivePlayer"
-  // TODO useless deck
+  override val deck = new TestDeck(this)
+  override def cardToDiscardForHandSizeLimit: Card = hand.head
+  override def chooseAction(actions: Seq[Action])(implicit gameState: GameState, phase: Phase) = actions.head
 }
