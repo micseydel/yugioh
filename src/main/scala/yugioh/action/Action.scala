180f66dcb72ae55ddac5cdfd0b1c303bcf61c4fc
Michael Seydel <micseydel@gmail.com>
2016-05-21 00:01:03 -0700

    Changes to Action, refactoring to look more like command pattern internet resources, added some default implementations, indirection of execute so that mulitple accidental calls can be detected. Discard inherent action added as well.

--- src/main/scala/yugioh/action/Action.scala
+++ src/main/scala/yugioh/action/Action.scala
@@ -2,15 +2,47 @@ package yugioh.action
 
 import yugioh.{GameState, Player}
 
-// TODO probably need implicit player
 trait Action {
-  def doAction()(implicit gameState: GameState, player: Player)
-  def undoAction()(implicit gameState: GameState)
+  private var called = false
+
+  def execute()(implicit gameState: GameState, player: Player): Unit = {
+    if (called) {
+      throw new IllegalStateException("Action " + this + " was already executed.")
+    }
+
+    doAction()
+    // TODO probably want to add to game state history here, emit an event
+    called = true
+  }
+
+  def undo()(implicit gameState: GameState): Unit = throw new NotImplementedError("Undo has not been implemented for " + this)
+
+  def redo()(implicit gameState: GameState): Unit = throw new NotImplementedError("Redo has not been implemented for " + this)
+
+  protected def doAction()(implicit gameState: GameState, player: Player): Unit
 }
 
-// TODO: pass priority
-
 trait InherentAction extends Action
+
+/**
+  * Composition of InherentAction(s).
+  */
 trait Activation extends Action
 
+trait PassPriority extends InherentAction
+
+class PassPriorityImpl extends PassPriority {
+  override def doAction()(implicit gameState: GameState, player: Player) = ()
+}
+
 trait Set extends InherentAction
+
+trait Discard extends InherentAction
+
+class DiscardImpl extends Discard {
+  override protected def doAction()(implicit gameState: GameState, player: Player) = {
+    val choice = player.cardToDiscardForHandSizeLimit
+    val card = player.hand.remove(player.hand.indexOf(choice))
+    card.owner.field.Graveyard.append(card)
+  }
+}
