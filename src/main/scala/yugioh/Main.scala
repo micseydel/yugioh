2981bba248011c8366e0e7b9994e321b47499654
Michael Seydel <micseydel@gmail.com>
2016-05-21 11:35:33 -0700

    [Main] Set opponents to players.

--- src/main/scala/yugioh/Main.scala
+++ src/main/scala/yugioh/Main.scala
@@ -2,9 +2,9 @@ package yugioh
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-
     val players: Seq[Player] = Seq(new CommandLineHumanPlayer("Human"), new PassivePlayer)
+    players.head.opponent = players.last
+    players.last.opponent = players.head
 
     val gameState = new GameStateImpl(players)
     gameState.mainLoop()
