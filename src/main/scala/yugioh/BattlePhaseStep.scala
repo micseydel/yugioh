e7a81f36eabd211a9a5670414e243b1efe7833b9
Michael Seydel <micseydel@gmail.com>
2016-06-05 20:54:10 -0700

    Refactored BattlePhaseStep to not have a default nex method or nextStep val (bcause that val was unused in the BattleStep, and the default next implementation didn't work without it).

--- src/main/scala/yugioh/BattlePhaseStep.scala
+++ src/main/scala/yugioh/BattlePhaseStep.scala
@@ -8,15 +8,10 @@ import yugioh.events._
 sealed trait Step
 
 sealed trait BattlePhaseStep extends Step {
-  protected val nextStep: BattlePhaseStep
-
-  def next(gameState: GameState): BattlePhaseStep = {
-    FastEffectTiming.loop(gameState.copy(step = this))
-    nextStep
-  }
-
   protected def emitStartEvent(): Unit
   protected def emitEndEvent(): Unit
+
+  def next(gameState: GameState): BattlePhaseStep
 }
 
 object BattlePhaseStep {
@@ -32,15 +27,16 @@ object BattlePhaseStep {
 }
 
 case object StartStep extends BattlePhaseStep {
-  override protected val nextStep: BattlePhaseStep = BattleStep
-
   override def emitStartEvent(): Unit = emit(StartStepStepStartEvent)
   override def emitEndEvent(): Unit = emit(StartStepStepEndEvent)
+
+  override def next(gameState: GameState): BattlePhaseStep = {
+    FastEffectTiming.loop(gameState.copy(step = this))
+    BattleStep
+  }
 }
 
 case object BattleStep extends BattlePhaseStep {
-  override protected val nextStep: BattlePhaseStep = null // not used here
-
   override def emitStartEvent(): Unit = emit(BattleStepStepStartEvent)
   override def emitEndEvent(): Unit = emit(BattleStepStepEndEvent)
 
@@ -71,22 +67,23 @@ case object BattleStep extends BattlePhaseStep {
 }
 
 case class DamageStep(attacker: Monster, target: Monster) extends BattlePhaseStep {
-  override protected val nextStep: BattlePhaseStep = BattleStep
-
   override def emitStartEvent(): Unit = emit(DamageStepStepStartEvent)
   override def emitEndEvent(): Unit = emit(DamageStepStepEndEvent)
 
   override def next(gameState: GameState): BattlePhaseStep = {
     DamageStepSubStep.loop(attacker, target)(gameState.copy(step = this))
-    nextStep
+    BattleStep
   }
 }
 
 case object EndStep extends BattlePhaseStep {
-  override protected val nextStep: BattlePhaseStep = null
-
   override def emitStartEvent(): Unit = emit(EndStepStepStartEvent)
   override def emitEndEvent(): Unit = emit(EndStepStepEndEvent)
+
+  override def next(gameState: GameState): BattlePhaseStep = {
+    FastEffectTiming.loop(gameState.copy(step = this))
+    null
+  }
 }
 
 
