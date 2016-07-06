45289407c74c6a997bc233354809b2f93100b72f
Michael Seydel <micseydel@gmail.com>
2016-07-05 20:50:33 -0700

    Starting Cake refactoring again, top down this time.

--- src/main/scala/yugioh/Main.scala
+++ src/main/scala/yugioh/Main.scala
@@ -2,14 +2,16 @@ package yugioh
 
 import yugioh.events.DefaultEventsComponent
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    val player1 = new CommandLineHumanPlayer("Human") with DefaultEventsComponent
-    val player2 = new PassivePlayer
+object Main extends DefaultPlayGameComponent
+    with DefaultEventsComponent {
+  val player1 = new CommandLineHumanPlayer("Human") with DefaultEventsComponent
+  val player2 = new PassivePlayer
 
-    val gameState = new PlayGameImpl((player1, player2)) with DefaultEventsComponent
+  override val Players = (player1, player2)
+
+  def main(args: Array[String]): Unit = {
     try {
-      gameState.mainLoop()
+      playGame.mainLoop()
     } catch {
       case gameLoss: GameLoss =>
         println(s"${gameLoss.loser} has lost! " + gameLoss)
--- src/main/scala/yugioh/PlayGame.scala
+++ src/main/scala/yugioh/PlayGame.scala
@@ -3,90 +3,97 @@ package yugioh
 import yugioh.action.monster.{DeclareAttack, NormalSummon, SetAsMonster}
 import yugioh.events.{BattleDamage, EventsComponent, TurnEndEvent, TurnStartEvent}
 
-trait PlayGame {
+trait PlayGameComponent {
   val Players: (Player, Player)
-  val IterablePlayers = Seq(Players._1,Players._2)
 
-  val mutableGameState: MutableGameState
+  trait PlayGame {
+    val IterablePlayers = Seq(Players._1,Players._2)
 
-  def mainLoop(): Unit
+    val mutableGameState: MutableGameState
+
+    def mainLoop(): Unit
+  }
+
+  def playGame: PlayGame
 }
 
-class PlayGameImpl(val Players: (Player, Player)) extends PlayGame {
+trait DefaultPlayGameComponent extends PlayGameComponent {
   self: EventsComponent =>
 
-  override val mutableGameState = new MutableGameState
+  override def playGame: PlayGame = new PlayGame {
+    override val mutableGameState = new MutableGameState
 
-  def mainLoop() = {
-    setupObservables()
+    def mainLoop() = {
+      setupObservables()
 
-    // before turns start, each player draws
-    for (player <- IterablePlayers) {
-      player.draw(Constants.InitialHandSize)
-    }
+      // before turns start, each player draws
+      for (player <- IterablePlayers) {
+        player.draw(Constants.InitialHandSize)
+      }
 
-    // loop until a game loss exception is thrown
-    // TODO LOW: catch the game loss exception here and do stuff based on it
-    for (turnPlayers <- playersCycle) {
-      takeTurn(turnPlayers)
-    }
-  }
-
-  private def takeTurn(turnPlayers: TurnPlayers) = {
-    mutableGameState.turnCount += 1
-
-    events.emit(TurnStartEvent(turnPlayers, mutableGameState))
-    Phase.loop(new GameState(mutableGameState, turnPlayers))
-    events.emit(TurnEndEvent)
-  }
-
-  /**
-    * An infinite cycle alternating between the two players.
-    */
-  private def playersCycle: Iterator[TurnPlayers] = {
-    val (player1, player2) = Players
-    Iterator.continually(Seq(TurnPlayers(player1, player2), TurnPlayers(player2, player1))).flatten
-  }
-
-  private def setupObservables() = {
-    // set hook for clearing turn state
-    events.observe { event =>
-      event match {
-        case TurnStartEvent(_, _) =>
-          mutableGameState.hasNormalSummonedThisTurn = false
-        case ignore =>
+      // loop until a game loss exception is thrown
+      // TODO LOW: catch the game loss exception here and do stuff based on it
+      for (turnPlayers <- playersCycle) {
+        takeTurn(turnPlayers)
       }
     }
 
-    // listen for a normal summon or set, flag that it happened
-    events.observe { event =>
-      event match {
-        case _:NormalSummon | _:SetAsMonster =>
-          mutableGameState.hasNormalSummonedThisTurn = true
-        case ignore =>
-      }
+    private def takeTurn(turnPlayers: TurnPlayers) = {
+      mutableGameState.turnCount += 1
+
+      events.emit(TurnStartEvent(turnPlayers, mutableGameState))
+      Phase.loop(new GameState(mutableGameState, turnPlayers))
+      events.emit(TurnEndEvent)
     }
 
-    // listen for an attack declaration, tag that monster as having attacked
-    events.observe { event =>
-      event match {
-        case attackDeclaration: DeclareAttack =>
-          for (monsterControlledState <- attackDeclaration.attacker.maybeMonsterControlledState) {
-            monsterControlledState.attackedThisTurn = true
-          }
-        case ignore =>
-      }
+    /**
+      * An infinite cycle alternating between the two players.
+      */
+    private def playersCycle: Iterator[TurnPlayers] = {
+      val (player1, player2) = Players
+      Iterator.continually(Seq(TurnPlayers(player1, player2), TurnPlayers(player2, player1))).flatten
     }
 
-    // listen for and handle life point damage
-    events.observe { event =>
-      event match {
-        case BattleDamage(player, damage) =>
-          player.lifePoints -= damage
-          if (player.lifePoints <= 0) {
-            throw OutOfLifepoints(player)
-          }
-        case ignore =>
+    private def setupObservables() = {
+      // set hook for clearing turn state
+      events.observe { event =>
+        event match {
+          case TurnStartEvent(_, _) =>
+            mutableGameState.hasNormalSummonedThisTurn = false
+          case ignore =>
+        }
+      }
+
+      // listen for a normal summon or set, flag that it happened
+      events.observe { event =>
+        event match {
+          case _:NormalSummon | _:SetAsMonster =>
+            mutableGameState.hasNormalSummonedThisTurn = true
+          case ignore =>
+        }
+      }
+
+      // listen for an attack declaration, tag that monster as having attacked
+      events.observe { event =>
+        event match {
+          case attackDeclaration: DeclareAttack =>
+            for (monsterControlledState <- attackDeclaration.attacker.maybeMonsterControlledState) {
+              monsterControlledState.attackedThisTurn = true
+            }
+          case ignore =>
+        }
+      }
+
+      // listen for and handle life point damage
+      events.observe { event =>
+        event match {
+          case BattleDamage(player, damage) =>
+            player.lifePoints -= damage
+            if (player.lifePoints <= 0) {
+              throw OutOfLifepoints(player)
+            }
+          case ignore =>
+        }
       }
     }
   }
