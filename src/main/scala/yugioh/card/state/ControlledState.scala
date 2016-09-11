cffc3297bb47d4cd78ea1fe9b106dcffa12952d5
Michael Seydel <micseydel@gmail.com>
2016-09-10 23:35:27 -0700

    Added TODO for renaming a state object.
    Added TODO for renaming ControlledState to IndentifiableState or something of the like, and explanation for reason. Deferring for now.

--- src/main/scala/yugioh/card/state/ControlledState.scala
+++ src/main/scala/yugioh/card/state/ControlledState.scala
@@ -3,6 +3,13 @@ package yugioh.card.state
 import yugioh.card.monster.Position
 import yugioh.events.{EventsModule, TurnEndEvent}
 
+/*
+ * TODO: perhaps ControlledState should be something more like IdentifiableState?
+ * For example, it usually applies to monsters not in the deck or hand. However, monsters which are in the grave
+ * or banished are *not* considered "controlled" so this isn't quite right. Also, a set monster usually doesn't
+ * have state, but if something like Geargiarmor flips then sets, it *does* retain some important state. Even
+ * non-effect monsters matter however, as in a monster set by Book of Moon after attacking or manually changing position.
+ */
 trait ControlledState {
   def faceup: Boolean
 
