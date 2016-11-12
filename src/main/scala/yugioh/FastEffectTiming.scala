982cbb979bc851298fb680df8c0cb6f9068887fb
Michael Seydel <micseydel@gmail.com>
2016-11-12 13:25:50 -0800

    Changed FastEffectTiming to use List instead of mutable.Stack.

--- src/main/scala/yugioh/FastEffectTiming.scala
+++ src/main/scala/yugioh/FastEffectTiming.scala
@@ -5,7 +5,6 @@ import yugioh.action.monster.DeclareAttack
 import yugioh.card.NonContinuousSpellOrTrap
 import yugioh.events._
 
-import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 
@@ -97,7 +96,7 @@ object OpenGameState extends FastEffectTiming {
 
     choice match {
       case pass: PassPriority => TryToEnd
-      case activation: Activation => ChainRules(mutable.Stack(activation), Nil)
+      case activation: Activation => ChainRules(List(activation), Nil)
       case attackDeclaration: DeclareAttack => null // BattlePhaseStep will change, manually go to ChainRules
       case action: InherentAction => CheckForTrigger(lastThingsToHappen.toList)
     }
@@ -126,7 +125,7 @@ case class TurnPlayerFastEffects(inResponseTo: List[Event]) extends PlayerFastEf
 
     choice match {
       case pass: PassPriority => OpposingPlayerFastEffects
-      case activation: Activation => ChainRules(mutable.Stack(activation), inResponseTo)
+      case activation: Activation => ChainRules(List(activation), inResponseTo)
       case _: InherentAction => throw new IllegalStateException("Inherent actions cannot be taken when game state is closed.")
     }
   }
@@ -145,7 +144,7 @@ object OpposingPlayerFastEffects extends PlayerFastEffects {
 
     choice match {
       case pass: PassPriority => OpenGameState
-      case activation: Activation => ChainRules(mutable.Stack(activation), Nil)
+      case activation: Activation => ChainRules(List(activation), Nil)
       case _: InherentAction => throw new IllegalStateException("Inherent actions cannot be taken by the opposing player.")
     }
   }
@@ -158,12 +157,14 @@ object OpposingPlayerFastEffects extends PlayerFastEffects {
   *                     or from B or C in the fast effect timing chart,
   *                     and MUST be None coming from E.
   */
-case class ChainRules(chain: mutable.Stack[Activation], inResponseTo: List[Event]) extends FastEffectTiming {
+case class ChainRules(startingChain: List[Activation], inResponseTo: List[Event]) extends FastEffectTiming {
   override def next(implicit gameState: GameState, eventsModule: EventsModule, actionModule: ActionModule) = {
     nextWithUpdatedGameState(gameState.copy(inResponseTo = inResponseTo), eventsModule, actionModule)
   }
 
   private def nextWithUpdatedGameState(implicit gameState: GameState, eventsModule: EventsModule, actionModule: ActionModule) = {
+    var chain = startingChain
+
     // listen to all events, and keep track of the "last thing to happen" which can include ***multiple*** events (e.g. Blackship of Corn)
     //   anytime a TimeSeparationEvent occurs, we drop all the old events
     // also listen for effect activation negation, and remove those effects from the chain
@@ -173,7 +174,7 @@ case class ChainRules(chain: mutable.Stack[Activation], inResponseTo: List[Event
         case TimeSeparationEvent =>
           lastThingsToHappen = new ListBuffer[Event]()
         case EffectActivationNegationEvent(_, _) =>
-          chain.pop()
+          chain = chain.tail
         case _ =>
           lastThingsToHappen.append(event)
       }
@@ -182,7 +183,7 @@ case class ChainRules(chain: mutable.Stack[Activation], inResponseTo: List[Event
     // build the chain
     var keepBuilding = true
     var passedPreviously = false
-    var player = gameState.turnPlayers.other(chain.top.player)
+    var player = chain.head.player.opponent
     while (keepBuilding) {
       player.chooseAction(actionsForPlayer(player)) match {
         case passPriority: PassPriority =>
@@ -191,14 +192,14 @@ case class ChainRules(chain: mutable.Stack[Activation], inResponseTo: List[Event
             keepBuilding = false
           } else {
             passedPreviously = true
-            player = gameState.turnPlayers.other(chain.top.player)
+            player = chain.head.player.opponent
           }
         case activation: Activation =>
           activation.execute()
           eventsModule.emit(TimeSeparationEvent)
-          chain.push(activation)
+          chain = activation :: chain
           passedPreviously = false
-          player = gameState.turnPlayers.other(chain.head.player)
+          player = chain.head.player.opponent
         case _: InherentAction => throw new IllegalStateException("Inherent actions cannot be taken when game state is closed.")
       }
     }
@@ -206,7 +207,12 @@ case class ChainRules(chain: mutable.Stack[Activation], inResponseTo: List[Event
     // resolve the chain
     val nonContinuousSpellTraps = new ListBuffer[NonContinuousSpellOrTrap]
     while (chain.nonEmpty) {
-      val activation: Activation = chain.pop()
+      val activation: Activation = {
+        val head = chain.head
+        chain = chain.tail
+        head
+      }
+
       activation.Effect.Resolution.execute()
       activation.Effect.Card.activated = false
 
@@ -259,7 +265,7 @@ object TryToEnd extends FastEffectTiming {
     eventsModule.emit(TimeSeparationEvent)
 
     choice match {
-      case activation: Activation => ChainRules(mutable.Stack(activation), Nil)
+      case activation: Activation => ChainRules(List(activation), Nil)
       case pass: PassPriority =>
         if (turnPlayers.turnPlayer.consentToEnd && turnPlayers.opponent.consentToEnd) {
           // hand size limit logic
@@ -304,7 +310,7 @@ case class CheckForTrigger(inResponseTo: List[Event]) extends FastEffectTiming {
   private def nextWithUpdatedGameState(implicit gameState: GameState, eventsModule: EventsModule, actionModule: ActionModule) = {
     // TODO: SEGOC exception - http://yugioh.wikia.com/wiki/Simultaneous_Effects_Go_On_Chain#Exception http://yugioh.wikia.com/wiki/Special_Summons_itself_from_your_hand
     // TODO: significant consideration must be made to conform properly to TCG's SEGOC rules
-    val segocChain = new mutable.Stack[Activation]
+    var segocChain = List[Activation]()
 
     // capture both of these functions to keep below code DRY
     //   first we ask turn player, then opposing player
@@ -326,7 +332,7 @@ case class CheckForTrigger(inResponseTo: List[Event]) extends FastEffectTiming {
 
           choice match {
             case activation: Activation =>
-              segocChain.push(activation)
+              segocChain = activation :: segocChain
               eventsModule.emit(TimeSeparationEvent)
             case pass: PassPriority =>
               // this is fine, we'll just exit at the end of the loop
